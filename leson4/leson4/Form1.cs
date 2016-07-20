b73d9c09e8e8f14dda9b4357e7133c369925621a
vluzhanskyi <Vluzhanskyi@gmail.com>
2016-07-20 18:51:01 +0300

    Sven

---
+++ leson4/leson4/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ApiCore;
+
+namespace leson4
+{
+    public partial class Form1 : Form
+    {
+        private ApiCore.SessionInfo sessionInfo;
+        private ApiCore.ApiManager manager;
+        private bool isLoggedIn = false;
+        private List<Friend> friendList;
+        private FriendsFactory friendFactory;
+        private Regex userIdCheck;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Reauth()
+        {
+            if (!this.isLoggedIn)
+            {
+                SessionManager sm = new SessionManager(5555068, Convert.ToInt32(ApiPerms.Audio | ApiPerms.ExtendedMessages | ApiPerms.ExtendedWall | ApiPerms.Friends | ApiPerms.Offers | ApiPerms.Photos | ApiPerms.Questions | ApiPerms.SendNotify | ApiPerms.SidebarLink | ApiPerms.UserNotes | ApiPerms.UserStatus | ApiPerms.Video | ApiPerms.WallPublisher | ApiPerms.Wiki));
+                this.sessionInfo = sm.GetSession(SessionAuthType.WithBrowser);
+                if (this.sessionInfo != null)
+                {
+                    this.isLoggedIn = true;
+                }
+            }
+
+            if (this.isLoggedIn)
+            {
+                manager = new ApiManager(this.sessionInfo);
+                manager.Timeout = 10000;
+                this.listBox1.Enabled = true;
+                this.friendFactory = new FriendsFactory(this.manager);
+                this.userIdCheck = new Regex("([\\d])+$");
+                this.GetFriendList();
+            }
+        }
+
+        //Загрузка списка друзей и запись данных в виджет listBox
+        private void GetFriendList()
+        {
+            try
+            {
+                this.listBox1.Items.Clear();
+                this.friendList = this.friendFactory.Get(FriendNameCase.Nominative, null, null, null, new string[] { "uid", "first_name", "nickname", "last_name" });
+                for (int i = 0; i < this.friendList.Count; i++)
+                {
+                    this.listBox1.Items.Add(this.friendList[i]);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error loading friend list:\n" + e.Message);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Вызов функций
+            this.Reauth();
+            this.GetFriendList();
+        }
+    }
+}
---
+++ leson4/leson4/Program.cs
@@ -0,0 +1,32 @@
+﻿using ApiCore;
+using MediaBrowser.Controller.Session;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VkAPI;
+using VkAPI.Wrappers.Friends;
+
+namespace leson4
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public class VKLogic
+    {
+
+    }
+}
